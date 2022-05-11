af62417930f40e83cefe2dedd0c0da849028610f
Vladislav ST <adrenaline2142@gmail.com>
2022-05-11 06:58:24 +0500

    Create film-cards-model.js

---
+++ src/model/film-cards-model.js
@@ -0,0 +1,10 @@
+import { MAX_CARDS } from '../const.js';
+import { generateFilm } from '../mock/film.js';
+
+export default class FilmCardsModel {
+  #cards = Array.from({length: MAX_CARDS}, generateFilm);
+
+  get cards() {
+    return this.#cards;
+  }
+}
