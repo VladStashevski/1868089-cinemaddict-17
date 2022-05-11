51bf012c00840cae8e329e5057ca76f3a109fb0e
Vladislav ST <adrenaline2142@gmail.com>
2022-05-11 06:59:34 +0500

    Create film-popup-form-view.js

---
+++ src/view/popup/film-popup-form-view.js
@@ -0,0 +1,23 @@
+import { createElement } from '../../render.js';
+
+const createFilmPopupTemplate = () => '<form class="film-details__inner" action="" method="get"></form>';
+
+export default class FilmPopupFormView {
+  #element = null;
+
+  get template() {
+    return createFilmPopupTemplate();
+  }
+
+  get element() {
+    if (!this.#element) {
+      this.#element = createElement(this.template);
+    }
+
+    return this.#element;
+  }
+
+  removeElement() {
+    this.#element = null;
+  }
+}
