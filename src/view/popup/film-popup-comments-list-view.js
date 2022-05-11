a85b651da571f1f4d4ac351f82f04c939f8a11c4
Vladislav ST <adrenaline2142@gmail.com>
2022-05-11 06:59:30 +0500

    Create film-popup-comments-list-view.js

---
+++ src/view/popup/film-popup-comments-list-view.js
@@ -0,0 +1,23 @@
+import { createElement } from '../../render.js';
+
+const createFilmPopupCommentsListTemplate = () => '<ul class="film-details__comments-list"></ul>';
+
+export default class FilmPopupCommentsListView {
+  #element = null;
+
+  get template() {
+    return createFilmPopupCommentsListTemplate();
+  }
+
+  get element() {
+    if (!this.#element) {
+      this.#element = createElement(this.template);
+    }
+
+    return this.#element;
+  }
+
+  removeElement() {
+    this.#element = null;
+  }
+}
