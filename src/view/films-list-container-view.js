64a6e6055128d9d8c87f10616b78a9f13b7aa240
Vladislav ST <adrenaline2142@gmail.com>
2022-05-11 06:59:04 +0500

    Create films-list-container-view.js

---
+++ src/view/films-list-container-view.js
@@ -0,0 +1,23 @@
+import {createElement} from '../render.js';
+
+const createFilmsListContainerTemplate = () => '<div class="films-list__container"></div>';
+
+export default class FilmsListContainerView {
+  #element = null;
+
+  get template() {
+    return createFilmsListContainerTemplate();
+  }
+
+  get element() {
+    if (!this.#element) {
+      this.#element = createElement(this.template);
+    }
+
+    return this.#element;
+  }
+
+  removeElement() {
+    this.#element = null;
+  }
+}
