11445dae28dd2a3e35ce144b709c696e01f75183
Vladislav ST <adrenaline2142@gmail.com>
2022-06-07 14:27:18 +0500

    Update film-card-view.js

--- src/view/film-card-view.js
+++ src/view/film-card-view.js
@@ -1,55 +1,66 @@
-import { createElement } from '../render.js';
-import { getReleaseYear, humanizeRuntime } from '../util.js';
+import AbstractView from '../framework/view/abstract-view.js';
+import {humanizeDueDate} from '../utils/tasks.js';
 
-const createFilmCardTemplate = (film) => {
-  const {comments, filmInfo} = film;
-  const {title, totalRating, poster, release, runtime, genre, description} = filmInfo;
-  const duration = humanizeRuntime(runtime);
+const createFilmCardTemplate = (movie) => {
 
-  return (
-    `<article class="film-card">
+  const {
+    filmInfo: {
+      title,
+      totalRating,
+      runtime,
+      poster,
+      description,
+      genre,
+      release: {
+        date
+      },
+    },
+    comments
+  } = movie;
+
+  return (`
+  <article class="film-card">
     <a class="film-card__link">
-     <h3 class="film-card__title">${title}</h3>
-     <p class="film-card__rating">${totalRating}</p>
-     <p class="film-card__info">
-      <span class="film-card__year">${getReleaseYear(release.date)}</span>
-      <span class="film-card__duration">${duration}</span>
-      <span class="film-card__genre">${genre.join(', ')}</span>
+      <h3 class="film-card__title">${title}</h3>
+      <p class="film-card__rating">${totalRating}</p>
+      <p class="film-card__info">
+        <span class="film-card__year">${humanizeDueDate(date)}</span>
+        <span class="film-card__duration">${runtime}</span>
+        <span class="film-card__genre">${genre}</span>
       </p>
-      <img src="${poster}" alt="" class="film-card__poster">
+      <img src="./${poster}" alt="${title}" class="film-card__poster">
       <p class="film-card__description">${description}</p>
       <span class="film-card__comments">${comments.length} comments</span>
     </a>
     <div class="film-card__controls">
-      <button class="film-card__controls-item film-card__controls-item--add-to-watchlist" type="button">Add to watchlist</button>
-      <button class="film-card__controls-item film-card__controls-item--mark-as-watched" type="button">Mark as watched</button>
-      <button class="film-card__controls-item film-card__controls-item--favorite" type="button">Mark as favorite</button>
+      <button class="film-card__controls-item film-card__controls-item--add-to-watchlist film-card__controls-item--active" type="button">Add to watchlist</button>
+      <button class="film-card__controls-item film-card__controls-item--mark-as-watched film-card__controls-item--active" type="button">Mark as watched</button>
+      <button class="film-card__controls-item film-card__controls-item--favorite film-card__controls-item--active" type="button">Mark as favorite</button>
     </div>
-    </article>`
-  );
+  </article>`);
 };
 
-export default class FilmCardView {
-  #element = null;
+export default class FilmCardView extends AbstractView {
+  #movie = null;
 
-  constructor(film) {
-    this.film = film;
+  constructor(movie) {
+    super();
+    this.#movie = movie;
   }
 
   get template() {
-    return createFilmCardTemplate(this.film);
+    return createFilmCardTemplate(this.#movie);
   }
 
-  get element() {
-    if (!this.#element) {
-      this.#element = createElement(this.template);
-    }
+  setClickHandler = (callback) => {
+    this._callback.click = callback;
+    this.element
+      .querySelector('.film-card__poster')
+      .addEventListener('click', this.#clickHandler);
+  };
 
-    return this.#element;
-  }
-
-  removeElement() {
-    this.#element = null;
-  }
+  #clickHandler = (evt) => {
+    evt.preventDefault();
+    this._callback.click();
+  };
 }
-
