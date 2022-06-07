1d8d7db8a7b5c413c524683ba53ad387ede64756
Vladislav ST <adrenaline2142@gmail.com>
2022-06-07 14:26:58 +0500

    Create footer-statistics-view.js

---
+++ src/view/footer-statistics-view.js
@@ -0,0 +1,13 @@
+import AbstractView from '../framework/view/abstract-view.js';
+
+const footerStatisticsElement = () => (`
+  <section class="footer__statistics">
+    <p>130 291 movies inside</p>
+  </section>
+`);
+
+export default class FooterStatisticsView extends AbstractView {
+  get template() {
+    return footerStatisticsElement();
+  }
+}
