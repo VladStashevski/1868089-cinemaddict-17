57d5b49c190c64407a5be2758426d14d85a43fb9
Vladislav ST <adrenaline2142@gmail.com>
2022-06-07 14:27:14 +0500

    Create film-nocard-view.js

---
+++ src/view/film-nocard-view.js
@@ -0,0 +1,22 @@
+import AbstractView from '../framework/view/abstract-view.js';
+
+const emptyMovieSection = () => (`
+  <section class="films">
+    <section class="films-list">
+      <h2 class="films-list__title">There are no movies in our database</h2>
+      <!--
+        Значение отображаемого текста зависит от выбранного фильтра:
+          * All movies – 'There are no movies in our database'
+          * Watchlist — 'There are no movies to watch now';
+          * History — 'There are no watched movies now';
+          * Favorites — 'There are no favorite movies now'.
+      -->
+    </section>
+  </section>
+`);
+
+export default class NoFilmCardView extends AbstractView {
+  get template() {
+    return emptyMovieSection();
+  }
+}
