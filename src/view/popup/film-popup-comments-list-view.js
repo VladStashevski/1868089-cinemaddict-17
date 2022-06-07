e6aa66772bd36c4935a38ab71ff097f8d66acd42
Vladislav ST <adrenaline2142@gmail.com>
2022-06-07 14:26:48 +0500

    Delete film-popup-comments-list-view.js

--- src/view/popup/film-popup-comments-list-view.js
+++
@@ -1,9 +0,0 @@
-import AbstractView from '../../framework/view/abstract-view.js';
-
-const createFilmPopupCommentsListTemplate = () => '<ul class="film-details__comments-list"></ul>';
-
-export default class FilmPopupCommentsListView extends AbstractView {
-  get template() {
-    return createFilmPopupCommentsListTemplate();
-  }
-}
