16ae15ba967320d6408d9d9d9002f9d23ba6e4fd
Vladislav ST <adrenaline2142@gmail.com>
2022-06-10 10:59:41 +0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import {render} from './framework/render.js';
+import {FilterType} from './const.js';
 import ProfileView from './view/profile-view.js';
 import MainNavigationView from './view/main-navigation-view.js';
 import FooterStatisticsView from './view/footer-statistics-view.js';
-import FilmsPresenter from './presenter/films-presenter-section.js';
-import MovieModel from './model/film-model.js';
+import FilmsPresenter from './presenter/main-presenter.js';
+import MovieModel from './model/movie-model.js';
 
 const pageHeader = document.querySelector('.header');
 const pageMain = document.querySelector('.main');
@@ -13,7 +14,7 @@ const movieModel = new MovieModel();
 const containerFilmsPresenter = new FilmsPresenter(pageMain, movieModel);
 
 render(new ProfileView, pageHeader);
-render(new MainNavigationView, pageMain);
-render(new FooterStatisticsView, pageFooter);
+render(new MainNavigationView(movieModel.filtered, FilterType.ALL), pageMain);
+render(new FooterStatisticsView(movieModel.count), pageFooter);
 
 containerFilmsPresenter.init();
