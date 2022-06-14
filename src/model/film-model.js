f8fe087369c53d8572844e839ebc1ee5faacdc72
Vladislav ST <adrenaline2142@gmail.com>
2022-06-14 09:58:05 +0500

    Update film-model.js

--- src/model/film-model.js
+++ src/model/film-model.js
@@ -1,23 +1,23 @@
 import {CARD_COUNT, FilterType} from '../const.js';
 import {genetateMovieCard} from '../mock/film.js';
 
-export default class MovieModel {
-  #movies = Array.from({length: CARD_COUNT}, genetateMovieCard);
+export default class FilmModel {
+  #films = Array.from({length: CARD_COUNT}, genetateMovieCard);
 
   get movie() {
-    return this.#movies;
+    return this.#films;
   }
 
   get count() {
-    return this.#movies.length;
+    return this.#films.length;
   }
 
   get filtered() {
     return ({
-      [FilterType.ALL]: () => this.#movies,
-      [FilterType.WATCHLIST]: () => this.#movies.filter((movie) => movie.userDetails.watchlist),
-      [FilterType.HISTORY]: () => this.#movies.filter((movie) => movie.userDetails.alreadyWatched),
-      [FilterType.FAVORITES]: () => this.#movies.filter((movie) => movie.userDetails.favorite)
+      [FilterType.ALL]: () => this.#films,
+      [FilterType.WATCHLIST]: () => this.#films.filter((movie) => movie.userDetails.watchlist),
+      [FilterType.HISTORY]: () => this.#films.filter((movie) => movie.userDetails.alreadyWatched),
+      [FilterType.FAVORITES]: () => this.#films.filter((movie) => movie.userDetails.favorite)
     });
   }
 }
