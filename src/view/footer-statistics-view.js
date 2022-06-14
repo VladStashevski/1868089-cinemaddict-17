fa36d7b99577027764192974de3a88459324e28f
Vladislav ST <adrenaline2142@gmail.com>
2022-06-14 09:58:10 +0500

    Update footer-statistics-view.js

--- src/view/footer-statistics-view.js
+++ src/view/footer-statistics-view.js
@@ -1,13 +1,11 @@
 import AbstractView from '../framework/view/abstract-view.js';
 
 const footerStatisticsElement = (movieCount) => (`
-  <section class="footer__statistics">
     <p>${movieCount} movies inside</p>
-  </section>
 `);
 
 export default class FooterStatisticsView extends AbstractView {
-  #movieCount = 0;
+  #movieCount = null;
 
   constructor(movieCount) {
     super();
