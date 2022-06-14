8bc3acae0d899abb29868bc3e2d03402eb9b3e77
Vladislav ST <adrenaline2142@gmail.com>
2022-06-14 09:58:03 +0500

    Правки по комментариям наставника

--- src/main.js
+++ src/main.js
@@ -3,18 +3,18 @@ import {FilterType} from './const.js';
 import ProfileView from './view/profile-view.js';
 import MainNavigationView from './view/main-navigation-view.js';
 import FooterStatisticsView from './view/footer-statistics-view.js';
-import FilmsPresenter from './presenter/main-presenter.js';
-import MovieModel from './model/film-model.js';
+import FilmsPresenter from './presenter/films-presenter.js';
+import FilmModel from './model/film-model.js';
 
 const pageHeader = document.querySelector('.header');
 const pageMain = document.querySelector('.main');
 const pageFooter = document.querySelector('.footer');
 
-const movieModel = new MovieModel();
-const containerFilmsPresenter = new FilmsPresenter(pageMain, movieModel);
+const filmModel = new FilmModel();
+const containerFilmsPresenter = new FilmsPresenter(pageMain, filmModel);
 
 render(new ProfileView, pageHeader);
-render(new MainNavigationView(movieModel.filtered, FilterType.ALL), pageMain);
-render(new FooterStatisticsView(movieModel.count), pageFooter);
+render(new MainNavigationView(filmModel.filtered, FilterType.ALL), pageMain);
+render(new FooterStatisticsView(filmModel.count), pageFooter);
 
 containerFilmsPresenter.init();
