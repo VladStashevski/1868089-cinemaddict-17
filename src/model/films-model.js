3f83755f9ec791d307401d56ee1b04aa8a249970
Vladislav ST <adrenaline2142@gmail.com>
2022-06-16 13:30:44 +0500

    Update films-model.js

--- src/model/films-model.js
+++ src/model/films-model.js
@@ -2,28 +2,28 @@ import Observable from '../framework/observable.js';
 import {CARD_COUNT} from '../const.js';
 import {generateFilmCard} from '../mock/film-template.js';
 
-export default class FilmModel extends Observable {
-  #films = Array.from({length: CARD_COUNT}, generateFilmCard);
+export default class MovieModel extends Observable {
+  #movies = Array.from({length: CARD_COUNT}, generateFilmCard);
 
-  get films() {
-    return this.#films;
+  get movies() {
+    return this.#movies;
   }
 
   get count() {
-    return this.#films.length;
+    return this.#movies.length;
   }
 
   updateFilm = (updateType, update) => {
-    const index = this.#films.findIndex((film) => film.id === update.id);
+    const index = this.#movies.findIndex((movie) => movie.id === update.id);
 
     if (index === -1) {
-      throw new Error('Can\'t update unexisting film');
+      throw new Error('Can\'t update unexisting movie');
     }
 
-    this.#films = [
-      ...this.#films.slice(0, index),
+    this.#movies = [
+      ...this.#movies.slice(0, index),
       update,
-      ...this.#films.slice(index + 1),
+      ...this.#movies.slice(index + 1),
     ];
 
     this._notify(updateType, update);
