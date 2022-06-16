20a708d3dc5c6802c8af4a34890becc833e75da4
Vladislav ST <adrenaline2142@gmail.com>
2022-06-16 13:30:45 +0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,21 +3,21 @@ import ProfileView from './view/profile-view.js';
 import FooterStatisticsView from './view/footer-statistics-view.js';
 import FilmsPresenter from './presenter/film-details-presenter.js';
 import FilterPresenter from './presenter/filter-presenter.js';
-import FilmModel from './model/films-model.js';
+import MovieModel from './model/films-model.js';
 import FilterModel from './model/filter-model.js';
 
 const pageHeader = document.querySelector('.header');
 const pageMain = document.querySelector('.main');
 const pageFooter = document.querySelector('.footer');
 
-const filmModel = new FilmModel();
+const movieModel = new MovieModel();
 const filterModel = new FilterModel();
-const containerFilmsPresenter = new FilmsPresenter(pageMain, filmModel, filterModel);
-const filterPresenter = new FilterPresenter(pageMain, filterModel, filmModel);
+const containerFilmsPresenter = new FilmsPresenter(pageMain, movieModel, filterModel);
+const filterPresenter = new FilterPresenter(pageMain, filterModel, movieModel);
 
 render(new ProfileView, pageHeader);
 
-render(new FooterStatisticsView(filmModel.count), pageFooter);
+render(new FooterStatisticsView(movieModel.count), pageFooter);
 
 filterPresenter.init();
 containerFilmsPresenter.init();
