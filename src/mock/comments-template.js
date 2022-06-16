8ad92b27c6394181be305081e6aa2c8c3ab68a41
Vladislav ST <adrenaline2142@gmail.com>
2022-06-16 11:58:18 +0500

    Create comments-template.js

---
+++ src/mock/comments-template.js
@@ -0,0 +1,18 @@
+import {getRandomInteger} from '../utils/common.js';
+
+const commentsEmotion = [
+  'smile',
+  'sleeping',
+  'puke',
+  'angry'
+];
+
+export const generateComment = () => ({
+  'id': getRandomInteger(1, 5),
+  'author': 'Ilya O Reilly',
+  'comment': 'a film that changed my life, a true masterpiece, post-credit scene was just amazing omg.',
+  'date': '2019-05-11T16:12:32.554Z',
+  'emotion': `${commentsEmotion[getRandomInteger(0, commentsEmotion.length-1)]}`
+});
+
+export {commentsEmotion};
