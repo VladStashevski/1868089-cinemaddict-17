7dab6b979660776e3162f7bf7c43b72e1398918a
Vladislav ST <adrenaline2142@gmail.com>
2022-06-16 11:59:12 +0500

    Create sort-view.js

---
+++ src/view/sort-view.js
@@ -0,0 +1,37 @@
+import AbstractView from '../framework/view/abstract-view.js';
+import {SortType} from '../const.js';
+
+const createFilterTemplate = (currentSortType) => (`
+  <ul class="sort">
+    <li><a href="#" data-sort-type="${SortType.DEFAULT}" class="sort__button ${currentSortType === SortType.DEFAULT ? 'sort__button--active' : ''}">Sort by default</a></li>
+    <li><a href="#" data-sort-type="${SortType.DATE}" class="sort__button ${currentSortType === SortType.DATE ? 'sort__button--active' : ''}">Sort by date</a></li>
+    <li><a href="#" data-sort-type="${SortType.RATING}" class="sort__button ${currentSortType === SortType.RATING ? 'sort__button--active' : ''}">Sort by rating</a></li>
+  </ul>
+`);
+
+export default class SortView extends AbstractView {
+  #currentSortType = null;
+
+  constructor(currentSortType) {
+    super();
+    this.#currentSortType = currentSortType;
+  }
+
+  get template() {
+    return createFilterTemplate(this.#currentSortType);
+  }
+
+  setSortTypeChangeHandler = (callback) => {
+    this._callback.sortTypeChange = callback;
+    this.element.addEventListener('click', this.#sortTypeChangeHandler);
+  };
+
+  #sortTypeChangeHandler = (evt) => {
+    if (evt.target.tagName !== 'A') {
+      return;
+    }
+
+    evt.preventDefault();
+    this._callback.sortTypeChange(evt.target.dataset.sortType);
+  };
+}
